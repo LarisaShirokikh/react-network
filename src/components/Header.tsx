dead104af59f2093facaeb3dd66d28efb20c6a51
larisasirokih <lorashirokikh@yandex.ru>
2022-10-11 13:27:08 +0300

    start

---
+++ src/components/Header.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+    return  <header className='header'>
+        <img
+            src='https://image.shutterstock.com/image-vector/creative-modern-img-letter-logo-260nw-1780496255.jpg'/>
+    </header>
+}
+
+export default Header;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://funkylife.in/wp-content/uploads/2021/06/good-morning-images-funkylife.jpg'/>
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+
+    </div>
+}
+
+export default Profile;

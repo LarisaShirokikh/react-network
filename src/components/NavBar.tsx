7533d4398c467fc2d0e6fb555f0e1cb07f9b0731
larisasirokih <lorashirokikh@yandex.ru>
2022-10-11 14:54:48 +0300

    header, profile, navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import './Navbar.css'
 
 const Navbar = () => {
    return  <nav className='nav'>
-        <div>
+        <div className='item'>
             <a>Profile</a>
         </div>
-        <div>
+        <div className='item'>
             <a>Messages</a>
         </div>
-        <div>
+        <div className='item'>
             <a>News</a>
         </div>
-        <div>
+        <div className='item'>
             <a>Music</a>
         </div>
     </nav>
